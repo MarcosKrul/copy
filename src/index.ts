990ea9f5876c4a6184843dfc7fc4faed03ee80a4
Marcos Renan Krul <marcoskrul2k@gmail.com>
2021-12-22 15:50:21 -0300

    server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log(process.env.PORT);
+import "reflect-metadata";
+import "express-async-errors";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+process.on("SIGTERM", () => {
+  process.exit();
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server started at ${process.env.PORT}`)
+);
+
+export { app };
